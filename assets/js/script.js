d676871da372fba2855879ce19e6c23e901684d6
masterwayne1988 <marcosanhuezao@gmail.com>
2024-07-03 22:39:38 -0400

    Primer comentario

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  //script JQuery / a traves del id(#), boton con id="enviarCorreo"
+   $("#enviarCorreo").click(function(){
+     alert("El correo fue enviado correctamente");
+   });
+  });
